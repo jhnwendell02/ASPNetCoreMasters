ba52da00455f765498359bb70b2a7ac009701113
John Wendell Custodio <WendellC@magenic.com>
2021-09-17 09:31:07 -0500

    Assignment on Session 2

---
+++ ASPNetCoreMastersTodoList.Api/Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMastersTodoList.Api.Controllers
+{
+
+    [ApiController]
+    public class ItemsController : Controller
+    {
+        [HttpGet]
+        [Route("/getall")]
+        public IEnumerable<string> GetAll(int userId)
+        {
+            ItemService service = new ItemService();
+            return service.GetAll(userId);
+        }
+    }
+}
---
+++ Services/ItemService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public class ItemService
+    {
+        public IEnumerable<string> GetAll(int userId)
+        {
+            return  new List<string>() { "jcustodio", "wcustodio", "jwcustodio" };
+        }
+    }
+}
