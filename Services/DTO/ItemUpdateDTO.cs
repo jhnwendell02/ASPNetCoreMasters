a7d64b81bf7f0f3b0dda26ef111d94752ea091f8
John Wendell Custodio <WendellC@magenic.com>
2021-09-24 03:37:28 -0500

    Session4 homework. (Routing)

---
+++ Services/DTO/ItemUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.DTO
+{
+    public class ItemUpdateDTO
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -2,19 +2,38 @@
 using Services.DTO;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Services
 {
     public class ItemService
     {
-        public int GetAll(int userId)
+        private readonly List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        public List<int> GetAll()
         {
-            return userId;
+            return numbers;
+
+        }
+        public int Get(int userId)
+        {
+            return numbers.Where(x => x == userId).FirstOrDefault();
         }
         public void Save(ItemDTO request)
         {
             Item item = new Item();
             item.Text = request.Text;
+        }
+        public void Update(ItemUpdateDTO request)
+        {
+            ItemUpdate item = new ItemUpdate() { Name = request.Name, Description = request.Description, ItemId = request.ItemId };
+        }
+        public void Create(ItemCreateDTO request)
+        {
+            ItemCreate item = new ItemCreate() { Name = request.Name, Description = request.Description, ItemId = request.ItemId };
+        }
+        public string Delete(int itemId)
+        {
+            return "Successfully deleted item number " + itemId.ToString();
         }
     }
 }
