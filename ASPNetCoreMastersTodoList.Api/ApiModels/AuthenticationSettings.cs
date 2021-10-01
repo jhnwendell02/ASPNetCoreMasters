e3f719213b5c2cb5d23945817b6d3b4714e50665
John Wendell Custodio <WendellC@magenic.com>
2021-10-01 07:54:51 -0500

    Session 5 homework (Configuration)

---
+++ ASPNetCoreMastersTodoList.Api/ApiModels/AuthenticationSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMastersTodoList.Api.ApiModels
+{
+    public class AuthenticationSettings
+    {
+        public string SecurityKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
